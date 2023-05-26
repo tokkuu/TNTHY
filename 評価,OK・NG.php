7f661c89cd5ec513b244ba455db73e56bb53baf4
Ayase25 <130340051+Ayase25@users.noreply.github.com>
2023-05-26 14:36:14 +0900

    画像
    うんこ

---
+++ 評価,OK・NG.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>評価,OK・NG</title>
+    </head>
+
+    <body>
+    <ul class="sidenav">
+
+        </ul>
+        <form action="" method="post">
+            <a class="active" href="#home">申請日</a><br>
+
+            <button type="submit" name="add">OK</button>
+            <button type="submit" name="delete">NG</button>
+        </form>
+
+    </body>
+</html>
